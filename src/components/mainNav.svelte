66d273c191b5df50f391b423b2b6fea0dfe79fbc
Alex Fraser <afraser281@gmail.com>
2023-11-06 20:11:29 -0400

    added main nav bar comonent

---
+++ src/components/mainNav.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+</script>
+
+<div class="navbar bg-base-200">
+    <div class="flex-1">
+        <a href="/">
+            <button class="btn btn-ghost">
+                LaunchPad
+            </button>
+        </a>
+    </div>
+    <div class="dropdown dropdown-end">
+        <div class="flex-none">
+            <button class="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </button>
+        </div>
+        <ul class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <li><p>+ Add Workspace</p></li>
+            <div class="divider"></div>
+            <li><a href="/">Settings</a></li>
+            <li><a href="/">Logout</a></li>
+        </ul>
+    </div>
+</div>
